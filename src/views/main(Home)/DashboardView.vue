6b6c0013fbeea97c3fea5b2249e732a96c821467
PorFavor90s <porploypradab555@gmail.com>
2023-03-16 09:30:49 +0700

    modified:   src/views/main(Home)/DashboardView.vue

--- src/views/main(Home)/DashboardView.vue
+++ src/views/main(Home)/DashboardView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="grid text-pink-200">
-    <h1>ยินดีต้อนรับ! สู่ระบบจัดการ Stock สินค้า NBA Digital Services Center</h1>
+    <h1>&nbsp;&nbsp;&nbsp;ยินดีต้อนรับ! สู่ระบบจัดการ Stock สินค้า NBA Digital Services Center</h1>
     </div>
     <div class="grid text-pink-200">
-    <h3>ส่วนนี้เป็นส่วนของหน้าแรก พื้นที่ตรงนี้สามารถปรับแต่งได้อย่างอิสระเลยนะคับ...
+    <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ส่วนนี้เป็นส่วนของหน้าแรก พื้นที่ตรงนี้สามารถปรับแต่งได้อย่างอิสระเลยนะคับ...
     </h3>
     </div>
 </template>
